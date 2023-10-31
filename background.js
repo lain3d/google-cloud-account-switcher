4a330cb6b8db95b9314648d64b1d5f6793b2b97a
lain3d <eyeofsteiner@gmail.com>
2023-10-31 12:17:08 +0700

    fix case where authUser with incorrect user is already in link

--- background.js
+++ background.js
@@ -1,23 +1,31 @@
 browser.webRequest.onBeforeRequest.addListener(
     async function (details) {
-        // console.log("running...");
-        const url = new URL(details.url);
-        console.log('running ' + url.hostname);
-        if (url.hostname.includes('console.cloud.google.com') && !url.searchParams.has('authuser')) {
-            try {
-                const result = await browser.storage.sync.get('authuser');
-            
-                console.log("checking authuser...");
-                if (result.authuser) {
-                    url.searchParams.append('authuser', result.authuser);
-                    console.log("appending authuser...");
+        try {
+            const result = await browser.storage.sync.get('authuser');
+            if (result.authuser) {
+                
+                const url = new URL(details.url);
+                
+                // Check if we're on a Google Cloud Console URL
+                if (url.hostname.includes('console.cloud.google.com')) {
+                    
+                    // Check if the URL's authuser matches the stored value
+                    if (url.searchParams.get('authuser') === result.authuser) {
+                        console.log("authuser already set correctly, no redirect needed");
+                        return;
+                    }
+                    
+                    // Otherwise, set or update the authuser parameter
+                    url.searchParams.set('authuser', result.authuser);
+                    
+                    console.log("redirecting to URL with updated authuser");
                     return { redirectUrl: url.href };
-                } else {
-                    console.log("authuser not set...");
                 }
-            } catch (error) {
-                console.error('Error fetching authuser', error);
+            } else {
+                console.log("authuser not set in storage");
             }
+        } catch (error) {
+            console.error('Error fetching authuser from storage', error);
         }
     },
     { urls: ["*://console.cloud.google.com/*"] },
